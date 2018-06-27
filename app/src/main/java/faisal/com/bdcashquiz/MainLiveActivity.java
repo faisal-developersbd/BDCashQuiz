8ce48916bf191824f3533883941d67e77173da3d
Tushar <tonyjpi69@gmail.com>
2018-06-27 16:30:19 +0600

    liveSessionDisconnect

--- app/src/main/java/faisal/com/bdcashquiz/MainLiveActivity.java
+++ app/src/main/java/faisal/com/bdcashquiz/MainLiveActivity.java
@@ -546,6 +546,7 @@ manager.beginTransaction().remove(framgentQuestion).commit();
     @Override
     public void onDisconnected(Session session) {
         Log.i(LOG_TAG, "Session Disconnected");
+        session.disconnect();
     }
 
     @Override
